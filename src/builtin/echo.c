6ce45b91feb98064606641dd7e9acd114acb1390
Raphael TISSERAND <rtissera@paul-f3Ar3s4.clusters.42paris.fr>
2023-11-03 14:54:40 +0100

    echo OK

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,34 +6,38 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 13:05:14 by rtissera          #+#    #+#             */
-/*   Updated: 2023/11/03 13:22:08 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/11/03 14:53:20 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "minishell.h"
-#include "../../inc/minishell.h"
+#include "minishell.h"
+
+int	nnn(char **array, int i)
+{
+	while (array[i])
+	{
+		if (array[i][0] && array[i][0] == '-')
+		{
+			j = 1;
+			while (array[i][j] && array[i][j] == 'n')
+				j++;
+			if (array[i][j])
+				return (i);
+		}
+		else
+			return (i);
+		i++;
+	}
+	return (i);
+}
 
 void	echo(char **array)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	while (array[i])
-	{
-		j = 0;
-		if (array[i][j] && array[i][j] == '-')
-		{
-			while (array[i][j] && array[i][j] == 'n')
-				j++;
-		}
-		else
-			break ;
-		if (array[i][j])
-			break ;
-		i++;
-	}
-	if (i > 0)
+	i = nnn(array, 0);
+	if (i != 0)
 	{
 		while (array[i])
 		{
@@ -55,3 +59,31 @@ void	echo(char **array)
 		}
 	}
 }
+/*
+int	main(int ac, char **av)
+{
+	int	i;
+	char	**array;
+
+	if (ac > 1)
+	{
+		array = malloc(sizeof(char *) * ac);
+		array[ac - 1] = NULL;
+		i = 0;
+		while (av[i + 1])
+		{
+			array[i] = malloc(sizeof(char) * (strlen(av[i + 1] + 1)));
+			strcpy(array[i], av[i + 1]);
+			i++;
+		}
+		echo(array);
+		i = 0;
+		while (array[i])
+		{
+			free(array[i]);
+			i++;
+		}
+		free(array);
+	}
+	return (0);
+}*/
