9f7638b2bbf88e185d0d4fbe7cb839185cdc6809
Raph <rtissera@student.42.fr>
2023-12-09 19:02:45 +0100

    clear main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 13:33:44 by rtissera          #+#    #+#             */
-/*   Updated: 2023/12/08 17:24:32 by rtissera         ###   ########.fr       */
+/*   Updated: 2023/12/09 19:00:34 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,10 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	**a;
 	t_env	*env_s;
 
 	(void)argc;
+	(void)argv;
 	env_s = env_init(envp);
-	ft_export(argv[1], env_s);
-	ft_export(NULL, env_s);
-	a = ft_split(argv[1], '=');
-	unset(a[0], env_s);
 	clear_env(env_s);
 }
