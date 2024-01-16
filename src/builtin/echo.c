60df8b5e5cea37e770aefc7379053e416d66511f
Raphael TISSERAND <rtissera@bess-f2r1s5.clusters.42paris.fr>
2024-01-16 16:35:05 +0100

    echo -n without anything else works

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: rtissera <rtissera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 13:05:14 by rtissera          #+#    #+#             */
-/*   Updated: 2024/01/10 16:17:50 by rtissera         ###   ########.fr       */
+/*   Updated: 2024/01/16 16:33:16 by rtissera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,7 @@ int	nnn(char **array, int i)
 			return (i);
 		i++;
 	}
-	return (1);
-}
-
-void	printpars(char *word, t_env *env)
-{
-	int		i;
-	char	*var;
-
-	i = 0;
-	while (word[i])
-	{
-		if (word[i] == '$')
-		{
-			var = ft_getenv(word + i + 1, env);
-			ft_putstr_fd(var, 1);
-			return ;
-		}
-		else
-			ft_putchar_fd(word[i], 1);
-		i++;
-	}
+	return (i);
 }
 
 void	echo(char **array, t_env *env)
@@ -58,13 +38,14 @@ void	echo(char **array, t_env *env)
 	int	i;
 	int	b;
 
+	(void)env;
 	i = nnn(array, 1);
 	b = 1;
 	if (i > 1)
 		b = 0;
 	while (array[i])
 	{
-		printpars(array[i], env);
+		ft_dprintf(1, "%s", array[i]);
 		if (array[i + 1])
 			ft_putchar_fd(' ', 1);
 		i++;
